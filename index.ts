0e18d72580544eaecb95cb36b542fe411d6033c9
Fly <723887809@qq.com>
2024-02-05 11:05:24 +0800

    Add TypeScript configuration file with strict mode disabled

---
+++ index.ts
@@ -0,0 +1,42 @@
+// let num:number = 666;// NaN Infinity
+// let str:string = `${num}`;
+// console.log(str); // 123
+
+
+// let notANumber: number = NaN;//Nan
+// let num1: number = 123;//普通数字
+// let infinityNumber: number = Infinity;//无穷大
+// let decimal: number = 6;//十进制
+// let hex: number = 0xd;//十六进制
+// let binary: number = 0b1010;//二进制
+// let octal: number = 0o744;//八进制s
+
+// console.log(notANumber);
+// console.log(num1);
+// console.log(infinityNumber);
+// console.log(decimal);
+// console.log(hex);
+// console.log(binary);
+// console.log(octal);
+
+// let b1:boolean = true;
+// let n:null = null;
+// let u:undefined = undefined;
+
+// // let v1:void = null;
+// let v2:void = undefined;
+
+
+// function myFn():void{
+//     console.log('hello world');
+// }
+// myFn();
+
+
+
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -10,3 +10,11 @@ tsc -v 安装成功打印版本
 ### 基础类型
 
 ### 任意类型
+
+
+
+## tsconfig.json
+```json
+    // 打开或关闭严格模式
+    "strict": false,   
+```
