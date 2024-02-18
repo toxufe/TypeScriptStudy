d16b969d5ec75a3da286443eebfc7dfda1169e50
Fly <723887809@qq.com>
2024-02-18 14:35:37 +0800

    update 213.ts"

--- 6.其他类型.ts
+++ 6.其他类型.ts
@@ -3,3 +3,18 @@
 
 // 类型断言
 // 语法：　　值 as 类型　　或　　<类型>值  value as string  <string>value
+
+interface People {
+    name: string
+    age: number
+}
+
+interface Man {
+    sex: string
+}
+
+let fnn = (man: People & Man): void => {
+    console.log(man);
+}
+
+fnn({ name: 'zhangsan', age: 20, sex: '男' });
